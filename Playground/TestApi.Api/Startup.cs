b5a5578473e8c86c775c81101b2653f2f4a88264
Rafael Fraga <ti.rafaelfraga@gmail.com>
2023-08-31 13:48:51 -0300

    Add extension methods

--- Playground/TestApi.Api/Startup.cs
+++ Playground/TestApi.Api/Startup.cs
@@ -1,11 +1,6 @@
-﻿using DotnetBoilerplate.Components.Api.Responses;
-using DotnetBoilerplate.Components.Application.Base;
-using DotnetBoilerplate.Components.Application.Pagination;
+﻿using DotnetBoilerplate.Components.Application;
 using DotnetBoilerplate.Components.Infra.Sql;
-using DotnetBoilerplate.Components.Shared.Notifications;
-using Microsoft.EntityFrameworkCore;
-using MongoDB.Driver.Core.Configuration;
-using System.Configuration;
+using DotnetBoilerplate.Components.Api;
 using TestApi.Infra.CrossCutting.IoC;
 using TestApi.Infra.Sql.Context;
 
@@ -24,16 +19,15 @@ namespace TestApi.Api
             services.AddControllers();
             services.AddSwaggerGen();
 
+            services.AddApi();
+            services.AddApplication();
             services.AddRepository();
             services.AddServiceApplication();
             services.AddMongoDb(Configuration);
 
-           services.AddDbContext<SqlContext>(Configuration);
+            services.AddDbContext<SqlContext>(Configuration);
 
-            services.AddScoped<NotificationContext>();
-            services.AddScoped<IResponseFactory, ResponseFactory>();
-            services.AddScoped(typeof(IPaginationResponse<>), typeof(PaginationResponse<>));
-            services.AddScoped<IBaseServiceApplication, BaseServiceApplication>();
+           
         }
 
         public void Configure(WebApplication app, IWebHostEnvironment environment)
