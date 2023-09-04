74fa56b06390abf6ddd0b3df9ac3257f311e5896
Rafael Fraga <ti.rafaelfraga@gmail.com>
2023-09-04 12:54:12 -0300

    Change the namespoce to the correct one

--- DotnetBoilerplate.Components.Domain.Sql/Dtos/Base/IBaseDto.cs
+++ DotnetBoilerplate.Components.Domain.Sql/Dtos/Base/IBaseDto.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Blog.Domain.Dtos.Base
+namespace DotnetBoilerplate.Components.Domain.Dtos.Base
 {
     public interface IBaseDto
     {
