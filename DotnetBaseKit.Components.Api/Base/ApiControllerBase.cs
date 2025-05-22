1a8dadc677bc2d3020811d0adffed6c01fabfa98
Rafael Fraga <tirafaelfraga@gmail.com>
2025-05-22 18:14:40 -0300

    Create BuildResponse method

--- DotnetBaseKit.Components.Api/Base/ApiControllerBase.cs
+++ DotnetBaseKit.Components.Api/Base/ApiControllerBase.cs
@@ -13,66 +13,56 @@ namespace DotnetBaseKit.Components.Api.Base
         {
             _responseFactory = responseFactory;
         }
+
+
+        private IActionResult BuildResponse(Response response, Func<object, IActionResult> successResult)
+        {
+            if (!response.Success)
+                return BadRequest(response);
+
+            return successResult(response);
+        }
+
         protected IActionResult CreateResponse()
         {
             var response = _responseFactory.Create();
 
-            if (!response.Success)
-                return BadRequest(response);
-
-            return Ok(response);
+            return BuildResponse(response, Ok);
         }
+
         protected IActionResult ResponseOk<TData>(TData result) where TData : class
         {
             var response = _responseFactory.Create(result);
 
-            if (!response.Success)
-                return BadRequest(response);
-
-            return Ok(response);
+            return BuildResponse(response, Ok);
         }
 
         protected IActionResult ResponseOk<TData>(IEnumerable<TData> result) where TData : class
         {
             var response = _responseFactory.Create(result);
-
-            if (!response.Success)
-                return BadRequest(response);
-
-            return Ok(response);
+            return BuildResponse(response, Ok);
         }
 
         protected IActionResult ResponseOk<TData>(PaginationResponse<TData> searchResult) where TData : class
         {
             var response = _responseFactory.Create(searchResult);
-
-            if (!response.Success)
-                return BadRequest(response);
-
-            return Ok(response);
+            return BuildResponse(response, Ok);
         }
 
         protected IActionResult ResponseCreated<TData>(TData result) where TData : class
         {
             var response = _responseFactory.Create(result);
 
-            if (!response.Success)
-                return BadRequest(response);
-
-            return Created("", response);
+            return BuildResponse(response, r => Created(string.Empty, r));
         }
 
         protected IActionResult ResponseCreated()
         {
             var response = _responseFactory.Create();
-
-            if (!response.Success)
-                return BadRequest(response);
-
-            return Created("", response);
-
+            return BuildResponse(response, r => Created(string.Empty, r));
         }
 
+            // BadRequest genérico (ainda que o retorno null não seja muito usual, pode ser ajustado)
         protected IActionResult ResponseBadRequest<TData>(TData result) where TData : class
         {
             var response = _responseFactory.Create(result);
@@ -81,7 +71,20 @@ namespace DotnetBaseKit.Components.Api.Base
                 return BadRequest(response);
 
             return null;
-
         }
+
+        protected IActionResult ResponseConflict<TData>(TData result) where TData : class
+        {
+            var response = _responseFactory.Create(result);
+            return Conflict(response);
+        }
+
+        protected IActionResult ResponseUnprocessableEntity<TData>(TData result) where TData : class
+        {
+            var response = _responseFactory.Create(result);
+
+            return UnprocessableEntity(response);
+        }
+
     }
 }
