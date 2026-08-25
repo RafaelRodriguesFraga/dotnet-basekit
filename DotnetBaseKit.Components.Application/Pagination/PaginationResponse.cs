ea6354373eba24e081aa3cd03fb6f7922f489ebb
Rafael Fraga <tirafaelfraga@gmail.com>
2026-08-25 17:16:29 -0300

    fix: ensure data property is initialized and validate constructor arguments

--- DotnetBaseKit.Components.Application/Pagination/PaginationResponse.cs
+++ DotnetBaseKit.Components.Application/Pagination/PaginationResponse.cs
@@ -8,17 +8,21 @@
         }
         public PaginationResponse(int currentPage, int quantityPerPage, long totalRecords, IEnumerable<TData> data)
         {
+            ArgumentOutOfRangeException.ThrowIfLessThan(currentPage, 1);
+            ArgumentOutOfRangeException.ThrowIfLessThan(quantityPerPage, 1);
+            ArgumentOutOfRangeException.ThrowIfNegative(totalRecords);
+
             CurrentPage = currentPage;
             QuantityPerPage = quantityPerPage;
             TotalRecords = totalRecords;
             TotalPages = (int)Math.Ceiling((double)totalRecords / (double)quantityPerPage);
-            Data = data;
+            Data = data ?? throw new ArgumentNullException(nameof(data));
         }
 
         public int CurrentPage { get; set; }
         public int QuantityPerPage { get; set; }
         public long TotalRecords { get; set; }
         public int TotalPages { get; set; }
-        public IEnumerable<TData> Data { get; set; }
+        public IEnumerable<TData> Data { get; set; } = Array.Empty<TData>();
     }
 }
